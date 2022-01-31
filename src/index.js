4984a7a460bf5f3a685f24ecabc705e7d2bd0f62
Daniel Berg <Berg.ioDaniel@gmail.com>
2022-01-31 14:25:52 +0100

    webpack setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = re
+const body = document.querySelector('body');
+var p = document.createElement('p');
+p.innerText = "Hello WOrld";
+body.appendChild(p);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { template } = require('lodash');
 const path = require('path');
-
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'WorldeSolver',
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.txt/,
+            type: 'asset/source',
+        }
+        ]
     },
 };
